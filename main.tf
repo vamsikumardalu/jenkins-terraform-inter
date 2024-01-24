d9e53d086eb98a271d5172316bd2d6307e0843ac
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2024-01-24 12:19:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my-instance" {
   count         = 3
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
-  key_name      = "ter"
+  key_name      = "todayvscode"
    tags = {
     Name  = "Terraform-${count.index + 1}"
   }
