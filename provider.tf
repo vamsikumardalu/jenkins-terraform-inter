a38d29191bc2e215ffdbf2478e8079d168b31434
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2024-01-25 22:53:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
+  region = "eu-central-1"
   access_key =  "AKIA32PH3AC5Y7QXBIPN"
   secret_key = "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"
 }
